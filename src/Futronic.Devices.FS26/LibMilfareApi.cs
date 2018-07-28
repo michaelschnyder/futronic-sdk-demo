380e418a008f96232a2af97e806b61d3ddf3d480
Michael Schnyder <michael.schnyder@zuehlke.com>
2018-07-29 00:41:17 +0800

    simplest implementation to read serial number from card

---
+++ src/Futronic.Devices.FS26/LibMilfareApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Futronic.Devices.FS26
+{
+    static class LibMilfareApi
+    {
+        [DllImport("ftrMFAPI.dll")]
+        internal static extern IntPtr ftrMFOpenDevice();
+
+        [DllImport("ftrMFAPI.dll")]
+        internal static extern void ftrMFCloseDevice(IntPtr handle);
+
+        [DllImport("ftrMFAPI.dll")]
+        internal static extern bool ftrMFStartSequence(IntPtr handle);
+
+        [DllImport("ftrMFAPI.dll")]
+        internal static extern bool ftrMFEndSequence(IntPtr handle);
+
+        [DllImport("ftrMFAPI.dll")]
+        internal static extern bool ftrMFActivateIdle8bSN(IntPtr handle, byte[] cardType, byte[] serialNumber);
+    }
+}
---
+++ src/ReadMilfareCardDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Futronic.Devices.FS26;
+
+namespace ReadMilfareCardDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LibMilfareApi Demo");
+
+            var accessor = new DeviceAccessor();
+
+            using (var device = accessor.OpenCardReader())
+            {
+                var sn = device.GetCardSerialNumber();
+
+                Console.WriteLine($"Serial number: {sn:D}, 0x{sn:X}");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
